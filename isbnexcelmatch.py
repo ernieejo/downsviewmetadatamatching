7a1a7dd354f466f0af389771d3aa8dfcd7f88d9a
Erin Johnson <36278045+ernieejo@users.noreply.github.com>
2019-08-20 12:44:08 -0400

    Delete isbnexcelmatch.py

--- isbnexcelmatch.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from pandas import DataFrame, read_excel, merge
-
-#matches against ISBN numbers
-df_1 = read_excel('filepath to [YOUR LIBRARY2.xlsx')
-df_2 = read_excel('filepath to utl2.xlsx')
-df_3 = df_1.merge(df_2, on='ISBN', how='left')
-df_3.to_excel('filepath to write isbnmatches.xlsx')
