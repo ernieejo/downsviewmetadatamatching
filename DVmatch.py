0ca20d59f8fe6c81bd2dccb5063f783a6b2cf167
Erin Johnson <36278045+ernieejo@users.noreply.github.com>
2019-08-20 15:40:33 -0400

    Update DVmatch.py

--- DVmatch.py
+++ DVmatch.py
@@ -4,11 +4,11 @@ from pandas import DataFrame, read_excel, merge, ExcelWriter
 #matches against OCLC numbers
 df_1 = read_excel('U:\\...WL-UTL data.xlsx', sheet_name='WL-OCLC')
 df_2 = read_excel('U:\\...WL-UTL data.xlsx', sheet_name='UTL-OCLC')
-df_3 = df_1.merge(df_2, on='OCLC', how='outer')
+df_3 = df_1.merge(df_2, on='OCLC', how='inner')
 df_4 = read_excel('U:\\...WL-UTL data.xlsx', sheet_name='WL-ISBN')
 df_5 = read_excel('U:\\...WL-UTL data.xlsx', sheet_name='UTL-ISBN')
-df_6 = df_4.merge(df_5, on='ISBN', how='outer')
-df_7 = df_3.merge(df_6, how='inner')
+df_6 = df_4.merge(df_5, on='ISBN', how='inner')
+df_7 = df_3.merge(df_6, how='outer')
 df_8 =df_4.merge(df_7, how='left')
 
 # writes a new spreadsheet
